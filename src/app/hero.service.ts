c01f3dd6ec3d0011efe9bae734d920f7bb92f014
Larry Fang <lfang615@gmail.com>
2018-12-11 14:09:45 -0600

    another commit

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,5 +14,15 @@ export class HeroService {
     return of(HEROES);
   }
 
+  // ngOnInit(): void {
+  //   this.getHero();
+  // }
+  
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
   constructor(private messageService: MessageService) { }
 }
